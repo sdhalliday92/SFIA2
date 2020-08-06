7c65f3072eb4efd4b7b4357741828d32865c68e0
sdhalliday92 <sdhalliday92@gmail.com>
2020-08-06 14:36:52 +0000

    working locally with database

--- Service_1/application/routes.py
+++ Service_1/application/routes.py
@@ -1,20 +1,38 @@
 from application import app
 from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from os import environ
 import requests
 import random
 
 
-class Superheroes(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name1 = db.Column(db.String(100), nullable=False)
-    name2 = db.Column(db.String(100), nullable=False)
+app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
+                                        environ.get('MYSQL_USER') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PASSWORD') + \
+                                        '@' + \
+                                        environ.get('MYSQL_HOST') + \
+                                        ':' + \
+                                        environ.get('MYSQL_PORT') + \
+                                        '/' + \
+                                        environ.get('MYSQL_DB_NAME')
+
+
+db = SQLAlchemy(app)
+
+
+class superheroes(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    heroname = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
     def __repr__(self):
         return ''.join([
-            'User ID: ', self.user_id, '\r\n',
-            'Name: ', self.name1, '\r\n', self.name2
+            'Name: ', self.heroname
         ])
 
 
@@ -23,5 +41,7 @@ def home():
     response = requests.get('http://service_4:5003/heroname')
     print(response)
     sentence = response.text
-    post_data = Superheroes.query.all()
+    post_data = superheroes.query.all()
+    db.session.add(superheroes( heroname = sentence ))
+    db.session.commit()
     return render_template('index.html', sentence = sentence, title = 'Home', superheroes=post_data)
--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-source .bashrc
+source ~/.bashrc
+
+pwd
 
 docker stack deploy --compose-file docker-compose.yml sfia2
