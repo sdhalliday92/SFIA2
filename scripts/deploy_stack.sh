1a76f82376b6727c322ccbd28c9319eab3092907
sdhalliday92 <sdhalliday92@gmail.com>
2020-08-07 09:40:09 +0000

    changed deploy stack script

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -4,11 +4,19 @@ source /home/jenkins/.bashrc
 
 pwd
 
-env  \ 
-MYSQL_USER="${MYSQL_USER}" \
-MYSQL_PASSWORD="${MYSQL_PASSWORD}" \
-MYSQL_HOST="${MYSQL_HOST}" \
-MYSQL_PORT="${MYSQL_PORT}" \
-MYSQL_DB_NAME="${MYSQL_DB_NAME}" \
-SECRET_KEY="${SECRET_KEY}" \
+# env  \ 
+# MYSQL_USER="${MYSQL_USER}" \
+# MYSQL_PASSWORD="${MYSQL_PASSWORD}" \
+# MYSQL_HOST="${MYSQL_HOST}" \
+# MYSQL_PORT="${MYSQL_PORT}" \
+# MYSQL_DB_NAME="${MYSQL_DB_NAME}" \
+# SECRET_KEY="${SECRET_KEY}" \
+
+sed -i "s/\${MYSQL_USER}/${MYSQL_USER}/g;" docker-compose.yml
+sed -i "s/\${MYSQL_PASSWORD}/${MYSQL_PASSWORD}/g;" docker-compose.yml
+sed -i "s/\${MYSQL_HOST}/${MYSQL_HOST}/g;" docker-compose.yml
+sed -i "s/\${MYSQL_PORT}/${MYSQL_PORT}/g;" docker-compose.yml
+sed -i "s/\${MYSQL_DB_NAME}/${MYSQL_DB_NAME}/g;" docker-compose.yml
+sed -i "s/\${SECRET_KEY}/${SECRET_KEY}/g;" docker-compose.yml
+
 docker stack deploy --compose-file docker-compose.yml sfia2
