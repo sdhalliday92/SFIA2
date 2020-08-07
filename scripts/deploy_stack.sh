f2dc11988ff069dfd147d0cf17a512e81ca793dd
sdhalliday92 <sdhalliday92@gmail.com>
2020-08-07 09:14:09 +0000

    added env variables to deploy stack script

--- scripts/deploy_stack.sh
+++ scripts/deploy_stack.sh
@@ -4,4 +4,11 @@ source /home/jenkins/.bashrc
 
 pwd
 
+env  \ 
+MYSQL_USER="${MYSQL_USER}" \
+MYSQL_PASSWORD="${MYSQL_PASSWORD}" \
+MYSQL_HOST="${MYSQL_HOST}" \
+MYSQL_PORT="${MYSQL_PORT}" \
+MYSQL_DB_NAME="${MYSQL_DB_NAME}" \
+SECRET_KEY="${SECRET_KEY}" \
 docker stack deploy --compose-file docker-compose.yml sfia2
